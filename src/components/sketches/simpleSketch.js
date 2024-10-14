9c66c32d3e3c8d97b0b9ef28bbc8f88d08203b21
Vicky9397 <vigneshwarana2712@gmail.com>
2024-10-14 18:51:56 +0530

    color changing

--- src/components/sketches/simpleSketch.js
+++ src/components/sketches/simpleSketch.js
@@ -1,6 +1,8 @@
 export const simpleSketch = (p) => {
   let pos = { x: 25, y: 25 }; // Initial position of the ball
   let speed = { x: 1, y: 1.5 }; // Initial speed in both x and y directions
+  let col = {r: 255, g: 255, b: 255}
+  let c = p.color(col.r, col.g, col.b);
 
   p.setup = () => {
     p.createCanvas(400, 400);
@@ -8,6 +10,7 @@ export const simpleSketch = (p) => {
 
   p.draw = () => {
     p.background(200);
+    p.fill(c);
     p.ellipse(pos.x, pos.y, 50, 50); // Draw the ball
 
     // Update ball position and handle collision
@@ -22,7 +25,22 @@ export const simpleSketch = (p) => {
 
   const handleCollision = () => {
     // Reverse direction on collision with walls
-    if (pos.x + 25 > p.width || pos.x - 25 < 0) speed.x *= -1;
-    if (pos.y + 25 > p.height || pos.y - 25 < 0) speed.y *= -1;
+    if (pos.x + 25 > p.width || pos.x - 25 < 0) 
+    { 
+      speed.x *= -1;
+      changeColor();
+    }
+    if (pos.y + 25 > p.height || pos.y - 25 < 0) 
+    {
+      speed.y *= -1;
+      changeColor();
+    }
   };
+
+  const changeColor = () => {
+    col.r = Math.floor(Math.random() * 256);
+    col.g = Math.floor(Math.random() * 256);
+    col.b = Math.floor(Math.random() * 256);
+    c = p.color(col.r, col.g, col.b);
+  }
 };
