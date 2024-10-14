e114314ecf68c241ca672ab9a1757a724577547e
Vicky9397 <vigneshwarana2712@gmail.com>
2024-10-14 18:35:12 +0530

    added stylings

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 import React from 'react';
 import P5Wrapper from './components/p5wrapper/P5wrapper';
 import { simpleSketch } from './components/sketches/simpleSketch';
+import './App.css'
 
 function App() {
   return (
-    <div>
+    <div className='app-container'>
       <h1>p5.js in React</h1>
       <P5Wrapper sketch={simpleSketch} />
     </div>
