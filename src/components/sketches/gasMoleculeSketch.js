355b7843fac5cb5c957c7f475c1e3d42122f910f
Vicky9397 <vigneshwarana2712@gmail.com>
2024-10-21 14:18:37 +0530

    minor fix for gas molecules

--- src/components/sketches/gasMoleculeSketch.js
+++ src/components/sketches/gasMoleculeSketch.js
@@ -4,12 +4,15 @@ export const gasMoleculeSketch = (p) => {
     class GasMolecule {
       constructor(x, y) {
         this.pos = p.createVector(x, y); // Use p.createVector instead of p5
-        this.vel = p.createVector(p.random(-2, 2), p.random(-2, 2)); // Random velocity
+        this.vel = p.createVector(p.random(-2, 2), p.random(-2, 2)); // Random initial velocity
+        this.speed = 1.5; // Speed of random movement
         this.radius = 10; // Size of each molecule
       }
   
       update() {
-        this.pos.add(this.vel); // Update position based on velocity
+        // Update ball position with random movement
+        this.pos.x += p.random(-1, 1) * this.speed;
+        this.pos.y += p.random(-1, 1) * this.speed;
   
         // Bounce off the walls (confined space)
         if (this.pos.x < this.radius || this.pos.x > p.width - this.radius) {
@@ -24,12 +27,12 @@ export const gasMoleculeSketch = (p) => {
         for (let other of otherMolecules) {
           if (this !== other) {
             let distance = p.dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y);
-            let minDistance = this.radius + other.radius + 1;
+            let minDistance = this.radius + other.radius;
   
             if (distance < minDistance) {
               let pushForce = p.createVector(this.pos.x - other.pos.x, this.pos.y - other.pos.y);
-              pushForce.setMag(0.5); // Push molecules apart
-              this.vel.add(pushForce); // Add force to velocity to push apart
+              pushForce.setMag(0.5); // Push molecules apart when too close
+              this.vel.add(pushForce); // Adjust velocity to move apart
             }
           }
         }
@@ -44,7 +47,7 @@ export const gasMoleculeSketch = (p) => {
   
     p.setup = () => {
       p.createCanvas(400, 400);
-      molecules.push(new GasMolecule(p.width / 2, p.height / 2)); // Initial gas molecule in the center
+      molecules.push(new GasMolecule(p.width / 2, p.height / 2)); // Initial gas molecule
     };
   
     p.draw = () => {
@@ -52,7 +55,7 @@ export const gasMoleculeSketch = (p) => {
   
       for (let molecule of molecules) {
         molecule.avoidCollision(molecules); // Avoid collision with other molecules
-        molecule.update(); // Update position
+        molecule.update(); // Update position with random movement
         molecule.display(); // Display molecule
       }
     };
